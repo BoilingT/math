1afb489b85a23bc3b16d4b408d05d048d4aa4899
Tobias <BoilingT@users.noreply.github.com>
2021-02-19 19:47:31 +0100

    Removed unecessary commented code

--- main.cpp
+++ main.cpp
@@ -18,22 +18,7 @@ void toArray(vec3 vector, float* out[1][3]) {
 	}
 }
 int main() {
-	//cout << list[0] << endl;
 	float angle = 0;
-	//std::list<float[3]> myList = { {0,0,0} };
-	
-	//myMatrix.create2DArray(2, 2);
-	//myMatrix.createMatrix(2, 2);
-	/*myMatrix.setMatrix(new float[3][3]{
-		{0, 0, 0},
-		{0, 0, 0},
-		{0, 0, 0}
-		});*/
-	//cout << myMatrix.toString() << endl;
-
-	//float** result = myMatrix.createMatrix<float>(3, 3);
-
-	vec3 row1 = { 1, 2, 3 };
 
 	matrix<2, 2> m2(new float[2][2]{
 		{2, 1},
@@ -48,16 +33,7 @@ int main() {
 	});
 	
 	cout << "m1: \n" << m1.toString() << endl; 
-	//cout << row1.toString() << endl;
 	cout << "Result: " << (m1.mult<2,2>(m2)).toString() << endl;
-
-	//cout << "Dot product: " << vec3(1, 0, 0).dotProduct(vec3(1, 1, 0), "degree") << endl;
-	
-	//cout << myMatrix.toString() << endl;
-	    
-	//cout << row1.toString() << endl;
-	//cout << row2.toString() << endl;
-	//cout << row1.getValues() << endl;
 	
 	system("PAUSE");
 	return 0;
